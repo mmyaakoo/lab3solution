c2fbb176cb59aef7a49256b69b47e2df47d91c62
Mark <Mark@WIN-K2879RTSAC4>
2023-12-03 18:26:06 +0200

    Добавьте файлы проекта.

---
+++ lab3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+
+class Program
+{
+    static void Main()
+    {
+        // Get input from the user
+        Console.Write("Enter the length of side 1: ");
+        double side1 = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Enter the length of side 2: ");
+        double side2 = Convert.ToDouble(Console.ReadLine());
+
+        // Create an instance of the Rectangle class
+        Rectangle rectangle = new Rectangle(side1, side2);
+
+        // Display rectangle information
+        Console.WriteLine($"Rectangle Perimeter: {rectangle.Perimeter}");
+        Console.WriteLine($"Rectangle Area: {rectangle.Area}");
+
+        // Keep the console window open
+        Console.ReadLine();
+    }
+}
